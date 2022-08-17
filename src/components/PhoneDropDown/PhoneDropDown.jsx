17dfacf88913f5e25b17d6978ce7fac3e68ee440
iremharnak <iremharnak@users.noreply.github.com>
2022-08-16 20:12:21 -0400

    removed bootstrap styling, will rebuild dropdown

--- src/components/PhoneDropDown/PhoneDropDown.jsx
+++ src/components/PhoneDropDown/PhoneDropDown.jsx
@@ -1,19 +1,9 @@
-import Dropdown from 'react-bootstrap/Dropdown';
+
 import './PhoneDropDown.css'
 
 export default function PhoneDropdown() {
   return (
-    <Dropdown className='dropdown'>
-      <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-        Phone
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">iPhone</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Pixel 4</Dropdown.Item>
-        
-      </Dropdown.Menu>
-    </Dropdown>
+    <h1>phone dropdown</h1>
   );
 }
 
