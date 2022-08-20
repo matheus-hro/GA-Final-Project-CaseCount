073e22f1882b03e9e4211735cc5a30700da48674
iremharnak <iremharnak@users.noreply.github.com>
2022-08-20 11:44:28 -0400

    added back to close modal btn

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,13 +3,14 @@ import CanvasBtn from '../CanvasBtn/CanvasBtn';
 import './Modal.css';
 
 export default function Modal (props){
+  const setOpenModal = props.setModalOpen;
 
   return(
     <div className='modal-overlay'>
       <div className='modal-container'>
         
       <div className='modal-body'>
-          <button className='close-modal-btn'>&times;</button>
+          <button className='close-modal-btn' onClick={()=> props.setOpenModal(false)}>&times;</button>
           <p>iPhone Case</p>
           <p>$24</p>
           </div>
